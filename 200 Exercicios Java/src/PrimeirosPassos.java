986c61b5d97f0b93dcf299b849018515617b4848
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-11-03 18:38:25 -0400

    Atualização

---
+++ 200 Exercicios Java/src/PrimeirosPassos.java
@@ -0,0 +1,9 @@
+public class PrimeirosPassos {
+    public static void main(String[] args) {
+        // Olá, Mundo!
+        System.out.println("Olá, Mundo!");
+
+        // Meu nome
+        System.out.println("Meu nome é Ronald!");
+    }
+}
