f30b7cb4746499d43d4c33bfd939b7c444e0895b
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-11-03 18:56:43 -0400

    atualização

---
+++ 200 Exercicios Java/src/Comentarios.java
@@ -0,0 +1,14 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Comentarios
+
+        /*
+         * comentario de bloco,
+         * varias linhas
+         */
+
+        /**
+         * documentação, javadoc
+         */
+    }
+}
