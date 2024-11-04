bf94851a409a8368de4ba19152bed4b739c85949
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-11-04 14:48:21 -0400

    atualização

---
+++ 200 Exercicios Java/src/Exercicio06_Constantes.java
@@ -0,0 +1,13 @@
+public class Exercicio06_Constantes {
+    public static void main(String[] args) {
+
+        // declarando a constante
+        final double velocidadeDaLuz = 299792458;
+
+        // alterando o valor da constante
+        // velocidadeDaLuz = 123456789; //descomente a linha
+
+        System.out.println("A velocidade da luz é: " + velocidadeDaLuz + " m/s");
+
+    }
+}
