ed5afbf53d57fff7a2a0441794a8b74d4973ab79
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-11-05 10:34:36 -0400

    atualização

---
+++ 200 Exercicios Java/src/Exercicio09_TiposDeVariaveis.java
@@ -0,0 +1,13 @@
+public class Exercicio09_TiposDeVariaveis {
+    // variavel global
+    static int variavelGlobal = 10;
+
+    public static void main(String[] args) {
+        // variavel local
+        int variavelLocal = 5;
+
+        System.out.println("A variavel global é: " + variavelGlobal);
+        System.out.println("A variavel local é: " + variavelLocal);
+
+    }
+}
