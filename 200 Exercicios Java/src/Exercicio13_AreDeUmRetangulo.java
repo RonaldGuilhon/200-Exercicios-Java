92bc69ba7152cf49b8a87b2cc3784473a06bbd42
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-11-06 13:01:11 -0400

    atualização

---
+++ 200 Exercicios Java/src/Exercicio13_AreDeUmRetangulo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercicio13_AreDeUmRetangulo {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("Digite a base do retângulo: ");
+        double base = sc.nextDouble();
+
+        System.out.printf("Digite a altura bdo retângulo: ");
+        double altura = sc.nextDouble();
+
+        double area = base * altura;
+
+        System.out.println("A area do retangulo é:  " + area);
+
+        sc.close();
+
+    }
+}
