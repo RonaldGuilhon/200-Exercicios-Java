62aa4732ced1386d4348adfcf8b0cea690145672
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-11-06 12:16:30 -0400

    atualização

--- 200 Exercicios Java/src/Exercicio12_MediaAritimetica.java
+++ 200 Exercicios Java/src/Exercicio12_MediaAritimetica.java
@@ -4,13 +4,13 @@ public class Exercicio12_MediaAritimetica {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite o primeiro  valor: ");
+        System.out.printf("Digite o primeiro  valor: ");
         int valor1 = scanner.nextInt();
 
-        System.out.println("Digite o primeiro  valor: ");
+        System.out.printf("Digite o primeiro  valor: ");
         int valor2 = scanner.nextInt();
 
-        System.out.println("Digite o primeiro  valor: ");
+        System.out.printf("Digite o primeiro  valor: ");
         int valor3 = scanner.nextInt();
 
         double media = (valor1 + valor2 + valor3) / 3;
