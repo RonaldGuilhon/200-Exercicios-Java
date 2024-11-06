6ddf4fa03bde1e23ec37ad626fad5c314832d0ab
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-11-06 12:13:13 -0400

    atualização

---
+++ 200 Exercicios Java/src/Exercicio11_CalculadoraSimples.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Exercicio11_CalculadoraSimples {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Digite um número: ");
+        int numero1 = scanner.nextInt();
+
+        System.out.printf("Digite outro número: ");
+        int numero2 = scanner.nextInt();
+
+        int soma = numero1 + numero2;
+        int subtracao = numero1 - numero2;
+        int multiplicacao = numero1 * numero2;
+        int divisao = numero1 / numero2;
+        int modulo = numero1 % numero2;
+
+        System.out.println("A soma dos valores é: " + soma);
+        System.out.println("A subtração dos valores é: " + subtracao);
+        System.out.println("A multilicação dos valores é: " + multiplicacao);
+        System.out.println("A divisão dos valores é: " + divisao);
+        System.out.println("O modulo dos valores é: " + modulo);
+
+    }
+}
---
+++ 200 Exercicios Java/src/Exercicio12_MediaAritimetica.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercicio12_MediaAritimetica {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro  valor: ");
+        int valor1 = scanner.nextInt();
+
+        System.out.println("Digite o primeiro  valor: ");
+        int valor2 = scanner.nextInt();
+
+        System.out.println("Digite o primeiro  valor: ");
+        int valor3 = scanner.nextInt();
+
+        double media = (valor1 + valor2 + valor3) / 3;
+
+        System.out.println("A média aritimetica é: " + media);
+
+    }
+}
