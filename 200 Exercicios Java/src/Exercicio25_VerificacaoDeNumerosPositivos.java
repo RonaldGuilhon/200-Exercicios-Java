aec6efbfba46b40e94f60119f85abcdfb1fe3d30
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-11-09 17:29:48 -0400

    atualização

---
+++ 200 Exercicios Java/src/Exercicio25_VerificacaoDeNumerosPositivos.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Exercicio25_VerificacaoDeNumerosPositivos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeir número interiro: ");
+        int numero1 = scanner.nextInt();
+
+        System.out.println("Digite o segundo número inteiro: ");
+        int numero2 = scanner.nextInt();
+
+        System.out.println("Digite o terceiro número inteiro: ");
+        int numero3 = scanner.nextInt();
+
+        int positivos = 0;
+
+        if (numero1 > 0)
+            positivos++;
+        if (numero2 > 0)
+            positivos++;
+        if (numero3 > 0)
+            positivos++;
+
+        if (positivos >= 2) {
+            System.out.println("Pelo menos dois números são positivos!");
+        } else {
+            System.out.println("Menos de dois números posistivos!");
+        }
+        scanner.close();
+
+    }
+}
