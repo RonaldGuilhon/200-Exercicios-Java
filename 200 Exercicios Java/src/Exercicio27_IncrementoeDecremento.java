e80b079215bfef4929456e4666b01e2cad47471e
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-11-10 10:44:46 -0400

    atualização

---
+++ 200 Exercicios Java/src/Exercicio27_IncrementoeDecremento.java
@@ -0,0 +1,16 @@
+public class Exercicio27_IncrementoeDecremento {
+    public static void main(String[] args) {
+        int x = 10;
+       
+        System.out.println("Valor inicial da variável x: " + x);
+
+        x++;
+        System.out.println("Valor da variável x após o incremento (x++): " + x);
+
+        x--;
+        System.out.println("Valor da variável x após o decremento (x--): " +x);
+
+
+    }
+
+}
