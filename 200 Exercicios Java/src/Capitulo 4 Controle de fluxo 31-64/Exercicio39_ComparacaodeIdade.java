ab1c1905af3824628c1e2e3da367bcadf07044f0
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-11-11 20:35:00 -0400

    atualização

--- 200 Exercicios Java/src/Capitulo 4 Controle de fluxo 31-64/Exercicio39_ComparacaodeIdade.java
+++ 200 Exercicios Java/src/Capitulo 4 Controle de fluxo 31-64/Exercicio39_ComparacaodeIdade.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Exercicio39_ComparacaodeIdade {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -9,14 +10,14 @@ public class Exercicio39_ComparacaodeIdade {
         System.out.printf("Digite a idade da segunda pessoa: ");
         int idadePessoa2 = scanner.nextInt();
 
-        if (idadePessoa1 > idadePessoa2){
+        if (idadePessoa1 > idadePessoa2) {
             System.out.printf("A primeira pessoa é mais velha que a segunda pessoa.\n");
-        } else if (idadePessoa1 < idadePessoa2){
+        } else if (idadePessoa1 < idadePessoa2) {
             System.out.printf("A segunda pessoa é mais velha que a primeira pessoa.\n");
-        } else if (idadePessoa1 == idadePessoa2){
+        } else if (idadePessoa1 == idadePessoa2) {
             System.out.printf("As duas pessoas têm a mesma idade.\n");
         }
-
+        scanner.close();
 
     }
 }
