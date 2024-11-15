7ab27338721447ad04d4e93bd796204fe53a2b6a
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-11-15 11:16:26 -0400

    atualização

---
+++ 200 Exercicios Java/src/Exercicio49_SomaDeNumeros.java
@@ -0,0 +1,11 @@
+public class Exercicio49_SomaDeNumeros {
+    public static void main(String[] args) {
+        int numero;
+        int soma = 0;
+
+        for (numero = 0; numero <= 100; numero ++){
+            soma = soma + numero;
+            System.out.println(soma);
+        }
+    }
+}
