9f16f2ad7b73e7554b571636fadad56bbf4b0de9
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-11-15 11:07:06 -0400

    atualização

---
+++ 200 Exercicios Java/src/Exercicio48_NumerosPares.java
@@ -0,0 +1,9 @@
+public class Exercicio48_NumerosPares {
+    public static void main(String[] args) {
+        int numerosPares;
+
+        for (numerosPares = 2; numerosPares <= 20; numerosPares += 2) {
+            System.out.println(numerosPares);
+        }
+    }
+}
