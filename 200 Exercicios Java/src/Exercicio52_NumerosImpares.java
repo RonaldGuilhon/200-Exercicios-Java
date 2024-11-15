6754b5238bcf5b9abf2efd542aff9448c8e0fa28
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-11-15 17:13:54 -0400

    atualização

---
+++ 200 Exercicios Java/src/Exercicio52_NumerosImpares.java
@@ -0,0 +1,10 @@
+public class Exercicio52_NumerosImpares {
+    public static void main(String[] args) {
+        int numerosImpares;
+
+        for (numerosImpares = 1; numerosImpares <= 50; numerosImpares +=2){
+            System.out.println(numerosImpares);
+        }
+    }
+
+}
