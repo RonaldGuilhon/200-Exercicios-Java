bb788e7180c7b48c234ddc730af3cc01c1084049
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-11-18 09:36:50 -0400

    atualização

---
+++ 200 Exercicios Java/src/Exercicio56_PordutoDeNumeros.java
@@ -0,0 +1,11 @@
+public class Exercicio56_PordutoDeNumeros {
+    public static void main(String[] args) {
+        int produto = 1;
+
+        for (int i = 1; i <= 10; i++) {
+            produto *= i;
+
+        }
+        System.out.println("O produto é: " + produto);
+    }
+}
