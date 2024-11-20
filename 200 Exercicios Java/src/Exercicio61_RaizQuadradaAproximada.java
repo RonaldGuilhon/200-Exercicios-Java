89a9ffca18a744c877a1b9f11756676685f37cd5
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-11-20 17:03:34 -0400

    atualização

--- 200 Exercicios Java/src/Exercicio61_RaizQuadradaAproximada.java
+++ 200 Exercicios Java/src/Exercicio61_RaizQuadradaAproximada.java
@@ -1,22 +1,20 @@
 import java.util.Scanner;
+
 public class Exercicio61_RaizQuadradaAproximada {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite um número inteiro positivo: ");
         int numeroInteiro = scanner.nextInt();
 
-        // Validação da entrada
         if (numeroInteiro < 0) {
             System.out.println("Por favor, insira um número inteiro positivo.");
         } else {
             int raizAprox = 0;
 
-            // Encontrar a raiz quadrada aproximada
             while (raizAprox * raizAprox < numeroInteiro) {
                 raizAprox++;
             }
 
-            // Verificar se é exata ou aproximada
             if (raizAprox * raizAprox == numeroInteiro) {
                 System.out.println("A raiz quadrada exata de " + numeroInteiro + " é " + raizAprox + ".");
             } else {
