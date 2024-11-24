d9858e896b614f5859b39d53982c280c4c253446
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-11-23 21:24:50 -0400

    atualização

---
+++ 200 Exercicios Java/src/Exercicio65_CriacaoInicializacaoArray.java
@@ -0,0 +1,10 @@
+public class Exercicio65_CriacaoInicializacaoArray {
+
+    public static void main(String[] args) {
+        int[] array = { 10, 20, 30, 40, 50 };
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Elemento " + i + ": " + array[i]);
+        }
+    }
+
+}
