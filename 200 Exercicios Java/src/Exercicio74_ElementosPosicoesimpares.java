46626886b7c079c145c85ee159f811e46502087e
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-12-01 13:02:31 -0400

    atualização

---
+++ 200 Exercicios Java/src/Exercicio74_ElementosPosicoesimpares.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercicio74_ElementosPosicoesimpares {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[10];
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.printf("Digite o " + (i + 1) + "º número: ");
+            numeros[i] = scanner.nextInt();
+        }
+        System.out.println("Os números digitados foram: ");
+        for (int i = 1; i < numeros.length; i += 2) {
+            System.out.println(numeros[i]);
+        }
+        scanner.close();
+    }
+
+}
