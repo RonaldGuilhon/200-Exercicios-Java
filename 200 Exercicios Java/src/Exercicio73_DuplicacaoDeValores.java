f24a8558875e135c67619f26bf34afe6a8b345d3
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-12-01 12:52:00 -0400

    atualização

--- 200 Exercicios Java/src/Exercicio73_DuplicacaoDeValores.java
+++ 200 Exercicios Java/src/Exercicio73_DuplicacaoDeValores.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class Exercicio73_DuplicacaoDeValores {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[5];
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Digite o " + (i + 1) + "º valor: ");
+            numeros[i] = scanner.nextInt();
+        }
+        System.out.println("Valores duplicados: ");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i] * 2);
+        }
+        scanner.close();
+    }
+
 }
