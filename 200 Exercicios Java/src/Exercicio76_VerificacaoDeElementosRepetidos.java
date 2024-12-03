29ef92cde65317940446fad480147413fe96fba1
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-12-03 18:30:24 -0400

    atualização

---
+++ 200 Exercicios Java/src/Exercicio76_VerificacaoDeElementosRepetidos.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercicio76_VerificacaoDeElementosRepetidos {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[10];
+        boolean repetido = false;
+        
+
+
+}
