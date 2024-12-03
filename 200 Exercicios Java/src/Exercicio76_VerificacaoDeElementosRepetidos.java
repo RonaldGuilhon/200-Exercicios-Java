b8e918c86c820ea691155e809bbbdbfc24a0e795
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-12-03 18:44:58 -0400

    atualização

--- 200 Exercicios Java/src/Exercicio76_VerificacaoDeElementosRepetidos.java
+++ 200 Exercicios Java/src/Exercicio76_VerificacaoDeElementosRepetidos.java
@@ -6,7 +6,23 @@ public class Exercicio76_VerificacaoDeElementosRepetidos {
         Scanner scanner = new Scanner(System.in);
         int[] numeros = new int[10];
         boolean repetido = false;
-        
-
-
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.printf("Digite o " + (i + 1) + "º número: ");
+            numeros[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = i + 1; j < numeros.length; j++) {
+                if (numeros[i] == numeros[j]) {
+                    repetido = true;
+                    break;
+                }
+            }
+        }
+        if (repetido) {
+            System.out.println("Há números repetidos.");
+        } else {
+            System.out.println("Não há números repetidos.");
+        }
+        scanner.close();
+    }
 }
