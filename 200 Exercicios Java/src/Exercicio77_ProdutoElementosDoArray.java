db5effb02c8d877039993d74eff565856d8bcc52
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-12-04 17:49:33 -0400

    atualização

--- 200 Exercicios Java/src/Exercicio77_ProdutoElementosDoArray.java
+++ 200 Exercicios Java/src/Exercicio77_ProdutoElementosDoArray.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 
 public class Exercicio77_ProdutoElementosDoArray {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[6];
+        int produto = 1;
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Digite o " + (i + 1) + "º número: ");
+            numeros[i] = scanner.nextInt();
+            produto *= numeros[i];
+        }
+        System.out.println("O produto dos números é: " + produto);
+        scanner.close();
     }
 
 }
