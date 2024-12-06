8d6eda024e9aa145c15f978a276251703e0e24fd
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-12-05 23:54:24 -0400

    atualização

---
+++ 200 Exercicios Java/src/Exercicio78_ComparacaoDeArrays.java
@@ -0,0 +1,20 @@
+public class Exercicio78_ComparacaoDeArrays {
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 3, 4, 5
+            };
+            int[] array2 = {1, 2, 3, 4, 5
+                };
+                System.out.println("Array 1: " + Arrays.toString(array1));
+                System.out.println("Array 2: " + Arrays.toString(array2));
+                \
+                if (Arrays.equals(array1, array2)) {
+                    System.out.println("Os arrays são iguais");
+                    } else
+                    System.out.println("Os arrays são diferentes");
+                    \
+                    }\
+}\}
+
+}
+
+}
