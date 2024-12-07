966b22dc81b3af24cf65cd49fcaa1c736ece6b33
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-12-07 15:14:09 -0400

    atualização

--- 200 Exercicios Java/src/Exercicio78_ComparacaoDeArrays.java
+++ 200 Exercicios Java/src/Exercicio78_ComparacaoDeArrays.java
@@ -9,13 +9,11 @@ public class Exercicio78_ComparacaoDeArrays {
         for (int i = 0; i < array1.length; i++) {
             System.out.println("Elemento [" + i + "] = " + array1[i]);
             array1[i] = scanner.nextInt();
-
         }
         System.out.println("Preencha o segundo array: ");
         for (int i = 0; i < array2.length; i++) {
             System.out.println("Elemento [" + i + "] = " + array2[i]);
             array2[i] = scanner.nextInt();
-
         }
         System.out.println("Comparação dos valores dos arrays na mesma posição:");
         for (int i = 0; i < array1.length; i++) {
@@ -25,5 +23,4 @@ public class Exercicio78_ComparacaoDeArrays {
         }
         scanner.close();
     }
-
 }
