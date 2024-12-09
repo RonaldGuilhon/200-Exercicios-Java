6452e72998f189b77aa2646352106656ce33fdeb
Ronald Simplicio <ronaldguilhon@gmail.com>
2024-12-08 20:26:55 -0400

    atualização

--- 200 Exercicios Java/src/Exercicio79_VerificacaoDeOrdemCrescente.java
+++ 200 Exercicios Java/src/Exercicio79_VerificacaoDeOrdemCrescente.java
@@ -3,10 +3,16 @@ import java.util.Scanner;
 public class Exercicio79_VerificacaoDeOrdemCrescente {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] emOrdem = new int[8];
-        for (int i = 0; i < emOrdem.length; i++) {
+        int[] numeros = new int[8];
+        boolean emOrdem = false;
+        for (int i = 0; i < numeros.length; i++) {
             System.out.println("Digite o " + (i + 1) + "º valor: ");
-            emOrdem[i] = scanner.nextInt();
+            numeros[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < numeros.length - 1; i++) {
+            if (condition) {
+
+            }
         }
 
     }
