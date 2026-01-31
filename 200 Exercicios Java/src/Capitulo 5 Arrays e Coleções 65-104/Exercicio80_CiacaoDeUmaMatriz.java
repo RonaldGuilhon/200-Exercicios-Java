f6f821d22d8b2c85e5b21e18295ceb456ce68468
Ronald Simplicio <ronaldguilhon@gmail.com>
2026-01-31 14:47:36 -0400

    atualização

---
+++ 200 Exercicios Java/src/Capitulo 5 Arrays e Coleções 65-104/Exercicio80_CiacaoDeUmaMatriz.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercicio80_CiacaoDeUmaMatriz {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] matriz = new int[3][4];
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.println("Digite o valor para a posição [" + i + "][" + j + "]: ");
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println("Matriz preenchida:");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
--- 200 Exercicios Java/src/Exercicio79_VerificacaoDeOrdemCrescente.java
+++ 200 Exercicios Java/src/Exercicio79_VerificacaoDeOrdemCrescente.java
@@ -4,22 +4,21 @@ public class Exercicio79_VerificacaoDeOrdemCrescente {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] numeros = new int[8];
-        boolean emOrdem = false;
+        boolean estaEmOrdem = true;
         for (int i = 0; i < numeros.length; i++) {
             System.out.println("Digite o " + (i + 1) + "º valor: ");
             numeros[i] = scanner.nextInt();
         }
         for (int i = 0; i < numeros.length - 1; i++) {
             if (numeros[i] > numeros[i + 1]) {
-                emOrdem = true;
+                estaEmOrdem = false;
                 break;
-                
             }
         }
-        if (emOrdem) {
-            System.out.println("Os números não estão em ordem crescente.");
-        } else {
+        if (estaEmOrdem) {
             System.out.println("Os números estão em ordem crescente.");
+        } else {
+            System.out.println("Os números não estão em ordem crescente.");
         }
         scanner.close();
     }
