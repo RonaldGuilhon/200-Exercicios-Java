907eaf1920e602c7160f64c38b5e72da573c5e31
Ronald Simplicio <ronaldguilhon@gmail.com>
2026-02-01 18:49:24 -0400

    atualização

--- 200 Exercicios Java/src/Exercicio85_MatrizTransposta.java
+++ 200 Exercicios Java/src/Exercicio85_MatrizTransposta.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class Exercicio85_MatrizTransposta {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] matriz = new int[3][3];
+        int[][] transposta = new int[3][3];
+
+        // preenchendo a matriz
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("Digite o valor para a posição [" + i + "][" + j + "]: ");
+                matriz[i][j] = scanner.nextInt();
+                transposta[j][i] = matriz[i][j];
+            }
+        }
+
+        // exibindo a matriz transposta
+        System.out.println("Matriz transposta:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(transposta[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
